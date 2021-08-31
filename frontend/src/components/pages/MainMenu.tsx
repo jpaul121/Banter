a7214103ced946276e926cb0473e022b153ce93c
jpaul121 <jpaulvalen+github@gmail.com>
2021-08-31 19:33:22 -0400

    Add menu screen

--- frontend/src/components/pages/MainMenu.tsx
+++ frontend/src/components/pages/MainMenu.tsx
@@ -1,28 +1,36 @@
-import '../../stylesheets/pages/Authentication.css'
+import '../../stylesheets/pages/MainMenu.css'
 
-import { faCommentAlt, faEnvelopeOpen, faInbox } from '@fortawesome/free-solid-svg-icons'
+import { faCommentAlt, faComments, faEnvelopeOpen, faInbox } from '@fortawesome/free-solid-svg-icons'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import Particles from 'react-tsparticles'
 import React from 'react'
 import particlesOptions from '../../particles.json'
+import { useHistory } from 'react-router-dom'
 
 function MainMenu() {
+  const history = useHistory()
+  
   return (
     <div className='menu-container'>
       <Particles id='tsparticles' options={particlesOptions} />
       <div className='menu-box'>
-        <div className='menu-item'>
+        <div className='menu-header'>
+          <FontAwesomeIcon icon={faComments} />
+          &nbsp;
+          Banter
+        </div>
+        <div className='menu-item' onClick={() => history.push('/inbox')}>
           <FontAwesomeIcon icon={faInbox} />
           &nbsp;
           Inbox
         </div>
-        <div className='menu-item'>
+        <div className='menu-item' onClick={() => history.push('/outbox')}>
           <FontAwesomeIcon icon={faEnvelopeOpen} />
           &nbsp;
           Outbox
         </div>
-        <div className='menu-item'>
+        <div className='menu-item' onClick={() => history.push('/compose')}>
           <FontAwesomeIcon icon={faCommentAlt} />
           &nbsp;
           Compose
